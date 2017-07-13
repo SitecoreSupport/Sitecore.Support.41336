160365d0f25d055566fe3f0f05eaa8221d141644
Dmitry Baranov <dbr@sitecore.net>
2017-07-13 15:49:33 +0300

    no message

---
+++ src/Sitecore.Support.41336/Modules/EmailCampaign/Core/Pipelines/CorrectOnHttpRequestBegin.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Resources.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines
+{
+    public class CorrectOnHttpRequestBegin
+    {
+        public void Process(HttpRequestArgs args)
+        {
+            if (MediaManager.IsMediaUrl(args.Context.Request.RawUrl))
+            {
+                Sitecore.Resources.Media.Media media = MediaManager.GetMedia(MediaManager.ParseMediaRequest(args.Context.Request).MediaUri);
+                if ((media != null) && !string.IsNullOrWhiteSpace(media.MimeType))
+                {
+                    args.Context.Response.ContentType = media.MimeType;
+                }
+            }
+        }
+    }
+}
